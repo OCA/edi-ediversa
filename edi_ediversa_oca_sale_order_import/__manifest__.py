600366e025ee7256158d12b73e77370902ddef05
Enric Tobella <enric.tobella@dixmit.com>
2025-08-28 08:21:42 +0200

    [MIG] edi_ediversa_oca_sale_order_import: Migration to 18

--- edi_ediversa_oca_sale_order_import/__manifest__.py
+++ edi_ediversa_oca_sale_order_import/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Ediversa - Sale Order Import",
     "summary": "Process sale orders from Ediversa",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "EDI",
     "website": "https://github.com/OCA/edi-ediversa",
     "author": "Sygel, Odoo Community Association (OCA)",
